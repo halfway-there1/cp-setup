c42be6f574bf7afd4eaa7efa9c556dddf8544baf
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2024-02-03 01:17:35 +0530

    Add debugging utility functions

---
+++ debugging.h
@@ -0,0 +1,31 @@
+#ifndef ANURAG_DEBUG
+#define ANURAG_DEBUG
+
+/* #include "bits/stdc++.h"
+using namespace std;
+
+using ll = int64_t;
+using ull = uint64_t;
+using lld = long double; */
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+
+void _print(ll t) {cerr << t;} // will also work for int
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#endif
